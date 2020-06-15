c7b818a59caa7413a909a94e784e012e0072b9c3
Unique Upreti <uniqueupreti44@gmail.com>
2020-06-15 16:36:13 -0400

    added a compinent

--- basketball-player/src/App.js
+++ basketball-player/src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+// import BasketballPlayerForm from "./components/BasketballPlayerForm";
+// import GamesPlayed from "./components/GamesPlayed";
+import { BasketballPlayerForm, GamesPlayed, BasketballTeamsContainer } from "./components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentPlayer: {},
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearCurrentPlayer = this.clearCurrentPlayer.bind(this);
+  }
+
+  handleSubmit(event, firstName, lastName) {
+    event.preventDefault();
+    axios
+      .get(
+        `https://nba-players.herokuapp.com/players-stats/${lastName}/${firstName}`
+      )
+      .then((res) => res.data)
+      .then((currentPlayer) => {
+        this.setState({ currentPlayer: currentPlayer });
+        console.log("currentPlayer on App State", currentPlayer);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  clearCurrentPlayer() {
+    this.setState({currentPlayer: {}});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {console.log("APP STATE", this.state)}
+        Full Name of Player: {this.state.currentPlayer.name}
+        <br></br>
+        Team Name of Player: {this.state.currentPlayer.team_name}
+
+        <BasketballPlayerForm handleSubmit={this.handleSubmit} clearCurrentPlayer={this.clearCurrentPlayer} />
+        <GamesPlayed gamesPlayed={this.state.currentPlayer.games_played} />
+        <BasketballTeamsContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
